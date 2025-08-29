55172b190dfd6dcf4d450705b73dad410b87c41e
luk-sulovsky <luk.sulovsky@gmail.com>
2025-08-29 22:22:10 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './App.css'; // jeśli masz plik CSS
+import './App.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
