bb2facf3eb6c117a9281e44302e28eb9dd52a5b1
luk-sulovsky <luk.sulovsky@gmail.com>
2025-10-28 08:59:17 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';  // Vite używa createRoot
 import App from './App.jsx';
-import './App.css'; 
+import './App.css';
 import * as Sentry from "@sentry/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Konfiguracja Sentry
+Sentry.init({
+  dsn: "https://39cd8b4ef3f2e8aa1832ebf7fca25664@o4510265954205696.ingest.de.sentry.io/4510265988350032",
+  environment: "production",
+  tracesSampleRate: 1.0,
+  sendDefaultPii: true
+});
+
+// Tworzymy root i renderujemy aplikację
+const container = document.getElementById('root');  // upewnij się, że w index.html masz <div id="root"></div>
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-Sentry.init({
-  dsn: "https://39cd8b4ef3f2e8aa1832ebf7fca25664@o4510265954205696.ingest.de.sentry.io/4510265988350032",
-  environment: "production",
-  tracesSampleRate: 1.0,
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true
-});
 
 
