2b5dc24f83f58eca228cd9f384f05ccc51098189
luk-sulovsky <luk.sulovsky@gmail.com>
2025-10-28 18:44:27 +0100

    commit

--- netlify/functions/health.js
+++ netlify/functions/health.js
@@ -1,5 +1,5 @@
 export async function handler() {
-  const monitorUrl = "https://o4510265954205696.ingest.de.sentry.io";
+  const monitorUrl = "https://o4510265954205696.ingest.de.sentry.io/api/4510265988350032/cron/hearbeat/39cd8b4ef3f2e8aa1832ebf7fca25664/";
 
   try {
     
