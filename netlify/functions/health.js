32dd7002ee21ed0ed4486647969ab869265ef5c1
luk-sulovsky <luk.sulovsky@gmail.com>
2025-10-28 18:25:34 +0100

    commit

--- netlify/functions/health.js
+++ netlify/functions/health.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 exports.handler = async function () {
   
   try {
-    await fetch("https://sentry.io/api/0/monitors/<TWÓJ_MONITOR_ID>/checkin/", {
+    await fetch("https://sentry.io/api/0/monitors/https://o4510265954205696.ingest.de.sentry.io/checkinhttps://o4510265954205696.ingest.de.sentry.io/api/4510265988350032/cron/heartbeat/39cd8b4e3f3208aa1832ebf7fca25664/", {
       method: "POST",
     });
   } catch (err) {
