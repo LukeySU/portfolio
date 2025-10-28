f155b4c3a357496c292b2defc2b38eda1e940655
luk-sulovsky <luk.sulovsky@gmail.com>
2025-10-28 10:07:11 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,        
-    environment: 'jsdom', 
-    setupFiles: './src/setupTests.js'
+    globals: true,         
+    environment: 'jsdom',  
+    setupFiles: './src/setupTests.js',
+    include: ['src/**/*.test.{js,ts,jsx,tsx}'] 
   }
 })
